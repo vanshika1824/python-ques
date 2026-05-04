78ea98a9f6aab4dd5c8ff1e10d64681c8081a329
Vanshika <vanshikamaru0205@gmail.com>
2026-05-04 12:50:20 +0530

    Add files via upload

---
+++ sorted.py
@@ -0,0 +1,11 @@
+tpl = (1,2,5,8,3)
+
+l = sorted(tpl)
+m = sorted(tpl,reverse=True)
+print(tpl)
+print(l)
+print(tuple(l))
+print(m)
+
+print(tpl.index(1))
+print(tpl.count(1))
---
+++ tuple.py
@@ -0,0 +1,11 @@
+'''tpl = (1,2,3)
+t1= tpl
+print(t1)
+print(type(tpl))
+
+tpl1=(1)
+print(type(tpl1))
+'''
+t2= tuple(input())
+print(type(t2))
+print(t2)
